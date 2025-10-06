5d3905b6b7981dcef18639404e62e19c1ab5fc22
Oluwatigbo <oluwatigboalao11@gmail.com>
2025-10-06 21:35:01 +0100

    ffg

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// config.js
+
+export const TMDB_API_KEY = 'cc5b03af4538b3527eff9204276833b2';
+export const YOUTUBE_API_KEY = 'AIzaSyDG3nKGapSyNFBSTutxRrlETPEA-soffWE';
+
+// Note: For security, do not expose API keys in public repos. Use environment variables or a backend proxy in production.
